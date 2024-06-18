1790f3c51928d026f77e3c8d3e1db75d1f33e80c
vineetkhemnani <vineetkhemnani09@gmail.com>
2024-06-18 20:29:35 +0530

    gemini api integrated

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-require('dotenv').config()
-const express = require('express')
-const axios = require('axios')
-const OpenAI = require('openai')
-const session = require('express-session')
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import axios from 'axios'
+// const OpenAI = require('openai')
+import session from 'express-session'
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 const app = express()
-app.use(express.static('public'))
-app.use(express.json())
+
+app.use(express.static('public')) // Use express.static to serve static files
+app.use(express.json()) // Use express.json to parse JSON bodies
 app.use(
   session({
     secret: 'your-secret-key-here',
@@ -15,8 +18,30 @@ app.use(
   })
 )
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// })
+const gemini_api_key = process.env.GEMINI_API_KEY
+
+
+const genAI = new GoogleGenerativeAI(gemini_api_key)
+
+// endpoint to handle gemini api response
+app.get('/', async (req, res) => {
+  try {
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' })
+
+    const prompt = 'Write a story about a magic backpack.'
+
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    const text = response.text()
+    // console.log(text)
+    res.json({text})
+  } catch (error) {
+    console.log('Error:', error)
+    res.status(500).json({ error: 'Failed to generate a response' })
+  }
 })
 
 const PORT = process.env.PORT || 3000
