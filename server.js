84fd541e1399e903304a85482c4b7b09880776ba
vineetkhemnani <vineetkhemnani09@gmail.com>
2024-06-19 13:51:27 +0530

    added frontend pages and routes

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import axios from 'axios'
+import path from 'path'
+import { fileURLToPath } from 'url'
 // const OpenAI = require('openai')
 import session from 'express-session'
 import { GoogleGenerativeAI } from '@google/generative-ai'
+import passport from 'passport'
+import { Strategy as GitHubStrategy } from 'passport-github'
 
 const app = express()
 
@@ -18,12 +22,75 @@ app.use(
   })
 )
 
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// })
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj)
+})
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: 'http://127.0.0.1:3000/auth/github/callback',
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      return cb(null, profile)
+    }
+  )
+)
+
+// route to handle github authentication
+app.get('/auth/github', passport.authenticate('github'))
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/')
+  }
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// serve login page if user not authenticated
+app.get('/', (req, res) => {
+  if (!req.isAuthenticated()) {
+    // if not authenticated go to login page
+    res.sendFile(path.join(__dirname, 'public', 'login.html'), (err) => {
+      if (err) {
+        res.status(500).send('Error occurred while serving the file.')
+      }
+    })
+  } else {
+    res.redirect('/home')
+  }
+})
+
+// protected route to serve homepage after login
+app.get('/home', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'), (err) => {
+      if (err) {
+        res.status(500).send('Error occurred while serving the file.')
+      }
+    })
+  } else {
+    res.redirect('/')
+  }
+})
+
+// gemini setup
 const gemini_api_key = process.env.GEMINI_API_KEY
-
-
 const genAI = new GoogleGenerativeAI(gemini_api_key)
 
 // endpoint to handle gemini api response
@@ -31,13 +98,31 @@ app.get('/', async (req, res) => {
   try {
     const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' })
 
-    const prompt = 'Write a story about a magic backpack.'
+    const chat = model.startChat({
+      history: [
+        {
+          role: 'user',
+          parts: [
+            { text: 'Suppose you are a witty AI with a good sense of humour' },
+          ],
+        },
+        {
+          role: 'user',
+          parts: [{ text: 'Your job is to roast me' }],
+        },
+      ],
+      generationConfig: {
+        maxOutputTokens: 100,
+      },
+    })
 
-    const result = await model.generateContent(prompt)
+    const msg = 'Roast me'
+
+    const result = await chat.sendMessage(msg)
     const response = await result.response
     const text = response.text()
     // console.log(text)
-    res.json({text})
+    res.json({ text })
   } catch (error) {
     console.log('Error:', error)
     res.status(500).json({ error: 'Failed to generate a response' })
